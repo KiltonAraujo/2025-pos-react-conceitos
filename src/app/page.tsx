0d85fbf6b36acffdfa6d28801091d8086c313ed4
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-05-05 14:28:01 +0000

    funfante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-// src/app/page.tsx
 "use client";
-
 import React from "react";
 
 const Titulo = () => (
@@ -19,39 +17,58 @@ function Cabecalho() {
 		</div>
 	);
 }
-
-interface TarefaProps {
-  titulo: string;
-	concluido?: boolean;
+interface TarefaInterface {
+	id: number;
+	Titulo: string;
+	Concluido: boolean;
 }
-class Tarefa extends React.Component<TarefaProps> {
-	
-	render(): React.ReactNode {
-		const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${this.props.concluido ? "bg-gray-800 hover:border-gray-800" : "bg-gray-400 hover:border-gray-400"}`;
 
-		return (
-			<div className={classe}>
-				<h3 className="text-xl font-bold">{this.props.titulo}</h3>
-				<p className="text-sm ">{this.props.concluido ? "Concluída" : "Pendente"}</p>
-			</div>
-		);
+interface TarefaProps{
+	dados: Array<TarefaInterface>;
+}
+
+const Tarefa: React.FC<TarefaInterface> = ({ Titulo, Concluido }) => {
+	const [estaConcluido, setEstaConcluido] = React.useState(Concluido);
+
+	const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
+		estaConcluido
+			? "bg-gray-800 hover:border-gray-800"
+			: "bg-gray-400 hover:border-gray-400"
+	}`;
+
+	const escutarClique = () => {
+		console.log(`A tarefa '${Titulo}' foi clicada!`);
+		setEstaConcluido(!estaConcluido);
 	}
-}
-
-const Home = () => {
-	const tarefas = [
-		{ id: 1, title: "delectus aut autem", completed: false },
-		{ id: 2, title: "quis ut nam facilis et officia qui", completed: true },
-		{ id: 3, title: "fugiat veniam minus", completed: false },
-	];
 
 	return (
-		<div className="container mx-auto p-4">
-			<Cabecalho />
-			<Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
-			<Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
+		<div
+			className={classe}
+			onClick={() => escutarClique()}
+		>
+			<h3 className="text-xl font-bold">{Titulo}</h3>
+			<p className="text-sm">{estaConcluido ? "Concluída" : "Pendente"}</p>
 		</div>
 	);
+};
+
+const Home = () => {
+    const tarefas = [
+        { id: 1, Titulo: "delectus aut autem", Concluido: false },
+        { id: 2, Titulo: "quis ut nam facilis et officia qui", Concluido: true },
+        { id: 3, Titulo: "fugiat veniam minus", Concluido: false },
+    ];
+
+    return (
+        <div className="container mx-auto p-4">
+            <Cabecalho />
+            <div>
+                {tarefas.map((tarefa) => (
+                    <Tarefa id={tarefa.id} Titulo={tarefa.Titulo} Concluido={tarefa.Concluido} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
